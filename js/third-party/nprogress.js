0bc93dc2e08c9bc4e889b16fe70680ab87e20ff4
proton <1163961922@qq.com>
2021-05-23 10:05:12 +0800

    Site updated: 2021-05-23 10:05:12

---
+++ js/third-party/nprogress.js
@@ -0,0 +1,2 @@
+/* global CONFIG, NProgress */
+NProgress.configure({showSpinner:CONFIG.nprogress.spinner}),NProgress.start(),document.addEventListener("readystatechange",()=>{"interactive"===document.readyState&&NProgress.inc(.8),"complete"===document.readyState&&NProgress.done()}),document.addEventListener("pjax:send",()=>{NProgress.start()}),document.addEventListener("pjax:success",()=>{NProgress.done()});
